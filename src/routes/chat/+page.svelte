8f444d89f77138e73052cc8c8fafe117aca8065b
Mav023 <gabrielluismaverick@gmail.com>
2024-08-29 01:33:34 +0800

    Improved: Chat

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,12 +1,38 @@
 <script lang="ts">
     import { chatStore } from "$lib/stores/chatStore";
+    import { writable } from "svelte/store";
     import ChatBubble from "$lib/components/chatBubble.svelte";
+
+    let newMessage = writable('');
+
+    function sendMessage() {
+        chatStore.update(store => {
+            store.messages.push({
+                sender: 'user',
+                message: $newMessage,
+                position: false
+            });
+            return store;
+        });
+        newMessage.set(''); // Clear the input field
+    }
 
 </script>
 
 
-<div>
-    {#each $chatStore.messages as message}
-        <ChatBubble sender={message['sender']} message={message['message']} position={message['position']} />
-    {/each}
+<div class=" bg-primary w-full h-screen flex flex-col justify-center items-center ">
+    <h1 class="text-4xl font-semibold text-secondary m-10">Lifeline</h1>
+    <div class="flex flex-col w-fit px-11 p-6 rounded-md bg-secondary">
+        <div class="flex flex-col">
+            {#each $chatStore.messages as message}
+                <div class="flex w-auto mb-3 {message['position'] ? 'justify-start': 'justify-end'}">
+                    <ChatBubble sender={message['sender']} message={message['message']} position={message['position']} />
+                </div>
+            {/each}
+        </div>
+        <div class="flex flex-row w-full px-3 py-2 rounded-full border border-gray-800 ">
+            <input type="text" bind:value={$newMessage} class="w-full px-2 py-1 mr-4 rounded-full" placeholder="Type a message..." />
+            <button class="bg-primary px-5 py-2 rounded-full text-secondary font-semibold" on:click={sendMessage}>send</button>
+        </div>  
+    </div>
 </div>
